fd21e06da861630687761a45144507b66e9e0014
Adam Antonio Kenning De La Peña <101642693+AdamKenning@users.noreply.github.com>
2026-08-25 14:12:44 +0100

    data mode img fix

--- main/myscript.user.js
+++ main/myscript.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         AboveInject
 // @namespace    https://github.com/AdamKenning
-// @version      2.7.0
+// @version      2.7.1
 // @description  Feature addition / QOL changes to the Survey page of Solargain
 // @author       Adam K
 
 // @match        https://analyst.abovesurveying.com/analystSurvey.php?*
 // @icon         https://analyst.abovesurveying.com/img/logo@2x.png
 
-// @resource mainCss https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/style.css?v=2.5.5
+// @resource mainCss https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/style.css?v=2.7.1
 // @grant GM_getResourceText
 // @grant GM_info
 
