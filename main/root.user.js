81d441e1658f8914ee5ace0ce8b76b261cd599b3
Adam Antonio Kenning De La Peña <101642693+AdamKenning@users.noreply.github.com>
2026-09-14 13:55:21 +0100

    2.0.0 root js maybe

--- main/root.user.js
+++ main/root.user.js
@@ -1,20 +1,28 @@
 // ==UserScript==
 // @name         Above.D.Inject
 // @namespace    https://github.com/AdamKenning
-// @version      1.0.1
+// @version      2.0.0
 // @description  Feature addition / QOL changes
 // @author       Adam K
 
-// @match        https://analyst*
+// @match        *://*/*
 // @icon         https://analyst.abovesurveying.com/img/logo@2x.png
 
-// @grant GM_getResourceText
-// @grant GM_info
-
-// @downloadURL https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/root.user.js
-// @updateURL   https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/root.user.js
+// @downloadURL https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/root.user.js
+// @updateURL   https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/root.user.js
 // ==/UserScript==
 
+const pageName = location.pathname.split('/').pop().replace('.php', '');
+
+const supportedPages = {
+    analystSurvey: {left: '15%'},
+    //analystPortal: {left: '25%'},
+    //analystAutoMapV2: {left: '10%'}
+};
+const pageConfig = supportedPages[pageName];
+if (!pageConfig) return;
+
+// Kill Switch
 function addKillSwitch(){
     const toggleBtn = document.createElement('button');
     toggleBtn.textContent = localStorage.getItem('disableInject') === 'true' ? 'Enable Inject' : 'Disable Inject';
@@ -30,7 +38,7 @@ function addKillSwitch(){
 
         position: fixed;
         top: 2px;
-        left: 15%;
+        left: ${pageConfig.left};
         transform: translateX(-50%);
         z-index: 999999;
     `;
@@ -44,22 +52,79 @@ function addKillSwitch(){
 }
 addKillSwitch()
 
-if(localStorage.getItem('disableInject') !== 'true'){
-    const routes = [
-        {
-            match: location.pathname.includes('analystSurvey.php'),
-            js: 'https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/survey/script.js',
-            css:'https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/survey/style.css'
+// Version info
+async function checkForUpdates(){
+    const VERSION = GM_info.script.version;
+    try{
+        const response = await fetch(
+            'https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/root.user.js?t=' + Date.now(), {cache: 'no-store'}
+        );
+        const text = await response.text();
+        const match = text.match(/@version\s+([0-9.]+)/);
+        if (!match) return;
+        const githubVersion = match[1];
+
+        console.log("Installed:", VERSION);
+        console.log("GitHub:", githubVersion);
+
+        const current = VERSION.split('.').map(Number);
+        const latest = githubVersion.split('.').map(Number);
+        const isPatchOnly = current[0] === latest[0] && current[1] === latest[1] && current[2] !== latest[2];
+
+        const btn = document.createElement('button');
+        btn.style.cssText = `
+                    background: #aaaaaa;
+                    color: #000000;
+                    border: 2px solid #000000;
+
+                    border-radius:4px;
+                    cursor:pointer;
+                    padding:4px 8px;
+                    min-width:100px;
+
+                    position: fixed;
+                    top: 2px;
+                    left: calc(${pageConfig.left} + 120px);
+                    transform: translateX(-50%);
+                    z-index: 999999;
+                `;
+
+        if(githubVersion !== VERSION){
+            btn.textContent = `v${VERSION} \u2794 v${githubVersion}`;
+            btn.title = `Installed: ${VERSION}\nLatest:     ${githubVersion}\nClick to update`;
+            btn.onclick = () => {window.open('https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/root.user.js','_blank');};
+        }else{
+            btn.textContent = `v${VERSION}`;
+            btn.title = `Installed: ${VERSION}\nLatest:     ${githubVersion}\nNo new updates`;
         }
-        // {
-        //     match: location.pathname.includes('analystAutoMapV2.php'),
-        //     js: 'https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/automap/script.js',
-        //     css: 'https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/automap/style.css'
-        // }
-    ];
-    const route = routes.find(r => r.match);
-    if(route){
-        loadCss(route.css);
-        loadJs(route.js);
-    }
+        if (document.body){document.body.appendChild(btn);}
+        else{window.addEventListener('DOMContentLoaded', () => {document.body.appendChild(btn);});}
+    }catch (err){console.error('Version check failed', err);}
+}
+checkForUpdates();
+
+if(localStorage.getItem('disableInject') !== 'true'){
+    const path = `https://raw.githubusercontent.com/AdamKenning/Above-Inject/main/main/${pageName}/` ;
+    const script_js_path = `${path}script.js`;
+    const style_css_path = `${path}style.css`;
+
+    // Fetch CSS
+    fetch(style_css_path + '?t=' + Date.now()).then(r => {
+        if (!r.ok) throw new Error(`CSS failed: ${r.status}`);
+        return r.text();
+    }).then(css => {
+        const style = document.createElement('style');
+        style.textContent = css;
+        document.head.appendChild(style);
+    }).catch(console.error);
+
+    // Fetch JS
+    fetch(script_js_path + '?t=' + Date.now()).then(r => {
+        if (!r.ok) throw new Error(`JS failed: ${r.status}`);
+        return r.text();
+    }).then(js => {
+        const script = document.createElement('script');
+        script.textContent = js;
+        document.head.appendChild(script);
+    }).catch(console.error);
 }
